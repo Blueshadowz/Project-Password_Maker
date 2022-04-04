c97a9a60d8b98459b414a4d4dc0912afbfe9daa4
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-04-04 22:52:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const dataNumbers = "0123456789";
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataSymbols = "~!@#$%^&*()-_=+[]{};:,.<>/?|";
+
+const generatePassword = () => {};
+
+btnSubmit.addEventListener("click", generatePassword);
