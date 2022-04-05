c451c3102d4df0fc61ef12ef5b7185333c6d4e8d
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-04-05 17:02:12 +0200

    Update Password Maker V1

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ const dataNumbers = "0123456789";
 const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
 const dataUppercase = dataLowercase.toUpperCase();
 const dataSymbols = "~!@#$%^&*()-_=+[]{};:,.<>/?|";
+const result = document.getElementById("result");
 
-const generatePassword = () => {};
+const generatePassword = () => {
+  const lengthValue = document.getElementById("length").value;
+  let data = [];
+  let password = "";
+
+  if (numbers.checked) data.push(...dataNumbers);
+  if (lowercase.checked) data.push(...dataLowercase);
+  if (uppercase.checked) data.push(...dataUppercase);
+  if (symbols.checked) data.push(...dataSymbols);
+
+  if (data.length === 0) {
+    alert("Veuillez sélectionner des options");
+    return;
+  } else {
+    for (let i = 0; i < lengthValue; i++) {
+      password += data[Math.floor(Math.random() * data.length)];
+    }
+  }
+
+  result.textContent = password;
+};
 
 btnSubmit.addEventListener("click", generatePassword);
